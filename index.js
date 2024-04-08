d69b45f6f5c7b10ebfda800e33d234eae5c0ec63
Piotr Banasik <piotr.banasik@gmail.com>
2024-04-08 16:01:48 -0700

    Switch to @kubernetes/client-node, add support for MODIFY event and bits of general cleanup

--- index.js
+++ index.js
@@ -1,15 +1,6 @@
-const KubeConfig = require('kubernetes-client').KubeConfig
-const Client = require('kubernetes-client').Client
-const JSONStream = require('json-stream')
-
-const kubeconfig = new KubeConfig()
-
-const configPath = process.argv[2] || '~/.kube/config'
-
-kubeconfig.loadFromFile(configPath)
-const Request = require('kubernetes-client/backends/request')
-const backend = new Request({ kubeconfig })
-const client = new Client({ backend, version: '1.13' })
+const k8s = require('@kubernetes/client-node');
+const kc = new k8s.KubeConfig();
+kc.loadFromDefault();
 
 const dbus = require('dbus-next');
 const bus = dbus.systemBus()
@@ -18,16 +9,16 @@ const hostname = process.argv[3]
 const namespace = process.argv[4] || "default"
 
 const cnames = {}
+const entriesByUid = {}
 
 process.on('SIGTERM', cleanup)
 process.on('SIGINT', cleanup)
 
-function cleanup() {
+async function cleanup() {
 	const keys = Object.keys(cnames)
 	for (k in keys) {
-		//console.log(keys[k])
-		cnames[keys[k]].Reset()
-    	cnames[keys[k]].Free()
+		await cnames[keys[k]].Reset()
+		await cnames[keys[k]].Free()
 	}
 	bus.disconnect()
 	process.exit(0)
@@ -53,61 +44,97 @@ function encodeFQDN(name) {
 	return buf
 }
 
-function main() {
+function removeItem(arr, value) { 
+  const index = arr.indexOf(value);
+  if (index > -1) {
+    arr.splice(index, 1);
+  }
+  return arr;
+}
+
+async function avahiAddAlias(server, host, uid) {
+	if (!cnames[host]) {
+		console.info("Adding", host)
+
+		let entryGroupPath = await server.EntryGroupNew()
+		let entryGroup = await bus.getProxyObject('org.freedesktop.Avahi',  entryGroupPath)
+		let entryGroupInt = entryGroup.getInterface('org.freedesktop.Avahi.EntryGroup')
+
+		var interface = -1
+		var protocol = -1
+		var flags = 0
+		var name = host
+		var clazz = 0x01
+		var type = 0x05
+		var ttl = 60
+		var rdata = encodeFQDN(hostname)
+
+		await entryGroupInt.AddRecord(interface, protocol, flags, name, clazz, type, ttl, rdata)
+		await entryGroupInt.Commit()
+		cnames[host] = entryGroupInt
+		if(!entriesByUid[uid]) {
+			entriesByUid[uid] = {}
+		}
+		if(!entriesByUid[uid][host]) {
+			entriesByUid[uid][host] = true
+		}
+	}
+}
+
+async function avahiDeleteAlias(host) {
+	if (cnames[host]) {
+		console.info("Deleting", host)
+		await cnames[host].Reset()
+		await cnames[host].Free()
+		delete cnames[host]
+	}
+}
+
+async function main() {
 
 	let server;
 
-	bus.getProxyObject('org.freedesktop.Avahi', '/')
-	.then( async obj => {
-		server = obj.getInterface('org.freedesktop.Avahi.Server')
-	})
+	const proxy = await bus.getProxyObject('org.freedesktop.Avahi', '/')
+	server = proxy.getInterface('org.freedesktop.Avahi.Server')
 
-	const stream = client.apis.extensions.v1beta1.namespaces(namespace).ingresses.getStream({qs:{ watch: true}})
+	const watch = new k8s.Watch(kc);
 
-	const jsonStream = new JSONStream()
-	stream.pipe(jsonStream)
-	jsonStream.on('data', async obj => {
-	    //console.log('Event: ', JSON.stringify(obj, null ,2))
-	    if (obj.type == "ADDED") {
-	    	for (x in obj.object.spec.rules) {
-	    		const host = obj.object.spec.rules[x].host
-	    		console.log("Adding: ", host)
+	await watch.watch(`/apis/networking.k8s.io/v1/namespaces/${namespace}/ingresses`, {}, async (phase, apiObj) => {
+		// console.info(phase, apiObj)
+		console.info(`${phase} - ${apiObj.metadata.namespace}/${apiObj.metadata.name}`)
 
-	    		if (!cnames[host]) {
-		    		let entryGroupPath = await server.EntryGroupNew()
-		    		let entryGroup = await bus.getProxyObject('org.freedesktop.Avahi',  entryGroupPath)
-		    		let entryGroupInt = entryGroup.getInterface('org.freedesktop.Avahi.EntryGroup')
-
-					var interface = -1
-					var protocol = -1
-					var flags = 0
-					var name = host
-					var clazz = 0x01
-					var type = 0x05
-					var ttl = 60
-					var rdata = encodeFQDN(hostname)
-
-					entryGroupInt.AddRecord(interface, protocol, flags, name, clazz, type, ttl, rdata)
-
-					entryGroupInt.Commit()
-					.then(()=>{
-						cnames[host] = entryGroupInt
-					})
+		if (phase == "ADDED") {
+			for (x in apiObj.spec.rules) {
+				const host = apiObj.spec.rules[x].host
+				await avahiAddAlias(server, host, apiObj.metadata.uid)
+			}
+		} else if (phase == "DELETED") {
+			const hostnames = Object.keys(entriesByUid[apiObj.metadata.uid])
+			console.info(hostnames)
+			for (x in apiObj.spec.rules) {
+				const host = apiObj.spec.rules[x].host
+				await avahiDeleteAlias()
+			}
+			delete entriesByUid[apiObj.metadata.uid]
+		} else if (phase == "MODIFIED") {
+			let hostnames = Object.keys(entriesByUid[apiObj.metadata.uid])
+			for (x in apiObj.spec.rules) {
+				const host = apiObj.spec.rules[x].host
+				hostnames = removeItem(hostnames, host)
+				if(!entriesByUid[apiObj.metadata.uid][host]) {
+					await avahiAddAlias(server, host, apiObj.metadata.uid)
 				}
-	    	}
-	    } else if (obj.type == "DELETED") {
-	    	for (x in obj.object.spec.rules) {
-	    		console.log("Removing: ", obj.object.spec.rules[x].host)
-	    		const host = obj.object.spec.rules[x].host
-	    		if (cname[host]) {
-		    		cnames[obj.object.spec.rules[x].host].Reset()
-		    		cnames[obj.object.spec.rules[x].host].Free()
-		    		delete cnames[obj.object.spec.rules[x].host]
-		    	}
-	    	}
-	    }
-		// console.log(cnames);
+			}
+			for (host of hostnames) {
+				await avahiDeleteAlias(host)
+			}
+		}
+
+	}, (err) => {
+		console.info(done, err)
 	})
 }
 
-main()
+main().catch((e) => {
+	console.error(e)
+})
